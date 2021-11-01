4d1d03768a8ca7ced19e4ab745d5bc600082f8ce
David Qian <david.qian02@gmail.com>
2021-11-01 17:29:30 -0400

    Complete Task 2 and 3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import CardEditor from './CardEditor';
+import CardViewer from './CardViewer';
 
-function App() {
-  return <div className="App">Testing</div>;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [
+        {front: 'front1', back: 'back1'},
+        {front: 'front2', back: 'back2'}
+      ],
+      editor: true,
+    };
+  }
+
+  addCard = card => {
+    const cards = this.state.cards.slice().concat(card);
+    this.setState({ cards });
+  };
+
+  deleteCard = index => {
+    const cards = this.state.cards.slice();
+    cards.splice(index, 1);
+    this.setState({ cards });
+  };
+
+  render() {
+    if (this.state.editor) {
+      return (
+        <CardEditor
+          addCard={this.addCard}
+          cards={this.state.cards}
+          deleteCard={this.deleteCard}
+          switchMode={this.switchMode}
+        />
+      );
+    } else {
+      return <CardViewer switchMode={this.switchMode} />;
+    }
+  }
+  
 }
 
 export default App;
