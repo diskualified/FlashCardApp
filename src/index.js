7ef81eed09941a1dbc307d72396d9ddffb124567
David Qian <david.qian02@gmail.com>
2021-11-08 18:54:24 -0500

    Download Devtools Extension

--- src/index.js
+++ src/index.js
@@ -11,48 +11,41 @@ import {
   ReactReduxFirebaseProvider,
   firebaseReducer,
 } from 'react-redux-firebase';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBlXFvDa0qUjN0L9iHvYPzifkAJKOMEapI",
-  authDomain: "bootcamp-2829f.firebaseapp.com",
-  databaseURL: "https://bootcamp-2829f-default-rtdb.firebaseio.com",
-  projectId: "bootcamp-2829f",
-  storageBucket: "bootcamp-2829f.appspot.com",
-  messagingSenderId: "1076609245376",
-  appId: "1:1076609245376:web:3079c756f4d9151eddcd25"
+    apiKey: "AIzaSyBlXFvDa0qUjN0L9iHvYPzifkAJKOMEapI",
+    authDomain: "bootcamp-2829f.firebaseapp.com",
+    databaseURL: "https://bootcamp-2829f-default-rtdb.firebaseio.com",
+    projectId: "bootcamp-2829f",
+    storageBucket: "bootcamp-2829f.appspot.com",
+    messagingSenderId: "1076609245376",
+    appId: "1:1076609245376:web:3079c756f4d9151eddcd25"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 // Add firebase to reducers
 const rootReducer = combineReducers({
     firebase: firebaseReducer,
-  });
+});
   
-  // Create store with reducers and initial state
-  const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
   
-  // react-redux-firebase config
-  const rrfConfig = {
+// react-redux-firebase config
+const rrfConfig = {
     userProfile: 'users',
-  };
+};
   
-  const rrfProps = {
+const rrfProps = {
     firebase,
     config: rrfConfig,
     dispatch: store.dispatch,
-  };
+};
   
 ReactDOM.render(
-    <Provider store={store}>
+  <Provider store={store}>
     <ReactReduxFirebaseProvider {...rrfProps}>
       <BrowserRouter>
         <App />
